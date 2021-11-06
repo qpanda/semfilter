e2c54fefaa3ff4a9d72ef74898f23f97396d0862
qpanda <7557807+qpanda@users.noreply.github.com>
2021-11-06 16:41:48 +0800

    feat: Improving Parser tests

--- src/parser.rs
+++ src/parser.rs
@@ -131,74 +131,118 @@ mod tests {
     #[test]
     fn from_text() {
         // setup
+        let text = "text";
         let text_token = Token {
             position: 0,
             separator: false,
-            text: String::from("text"),
+            text: String::from(text),
         };
         let tokens = vec![text_token];
 
         // exercise
-        let text_result = Value::from(&tokens, &Class::Text);
-        let integer_result = Value::from(&tokens, &Class::Integer);
-        let float_result = Value::from(&tokens, &Class::Float);
+        let text_results = Value::from(&tokens, &Class::Text);
+        let integer_results = Value::from(&tokens, &Class::Integer);
+        let float_results = Value::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(1, text_result.len()); // TODO match precisely
-        assert_eq!(0, integer_result.len());
-        assert_eq!(0, float_result.len());
+        assert_eq!(1, text_results.len());
+        let text_result = text_results.get(0).unwrap();
+        let expected_position = 0;
+        let expected_value = Value::new(&String::from(text), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
+
+        assert_eq!(0, integer_results.len());
+
+        assert_eq!(0, float_results.len());
     }
 
     #[test]
     fn from_integer() {
         // setup
+        let text = "8";
         let integer_token = Token {
             position: 0,
             separator: false,
-            text: String::from("8"),
+            text: String::from(text),
         };
         let tokens = vec![integer_token];
 
         // exercise
-        let text_result = Value::from(&tokens, &Class::Text);
-        let integer_result = Value::from(&tokens, &Class::Integer);
-        let float_result = Value::from(&tokens, &Class::Float);
+        let text_results = Value::from(&tokens, &Class::Text);
+        let integer_results = Value::from(&tokens, &Class::Integer);
+        let float_results = Value::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(1, text_result.len()); // TODO match precisely
-        assert_eq!(1, integer_result.len()); // TODO match precisely
-        assert_eq!(1, float_result.len()); // TODO match precisely
+        assert_eq!(1, text_results.len());
+        let text_result = text_results.get(0).unwrap();
+        let expected_position = 0;
+        let expected_value = Value::new(&String::from(text), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
+
+        assert_eq!(1, integer_results.len());
+        let integer_result = integer_results.get(0).unwrap();
+        let expected_position = 0;
+        let expected_value = Value::new(&String::from(text), &Class::Integer).unwrap();
+        assert_eq!(expected_position, integer_result.0);
+        assert_eq!(expected_value, integer_result.1);
+
+        assert_eq!(1, float_results.len());
+        let float_result = float_results.get(0).unwrap();
+        let expected_position = 0;
+        let expected_value = Value::new(&String::from(text), &Class::Float).unwrap();
+        assert_eq!(expected_position, float_result.0);
+        assert_eq!(expected_value, float_result.1);
     }
 
     #[test]
     fn from_float() {
         // setup
+        let text = "5.5";
         let float_token = Token {
             position: 0,
             separator: false,
-            text: String::from("5.5"),
+            text: String::from(text),
         };
         let tokens = vec![float_token];
 
         // exercise
-        let text_result = Value::from(&tokens, &Class::Text);
-        let integer_result = Value::from(&tokens, &Class::Integer);
-        let float_result = Value::from(&tokens, &Class::Float);
+        let text_results = Value::from(&tokens, &Class::Text);
+        let integer_results = Value::from(&tokens, &Class::Integer);
+        let float_results = Value::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(1, text_result.len()); // TODO match precisely
-        assert_eq!(0, integer_result.len()); // TODO match precisely
-        assert_eq!(1, float_result.len()); // TODO match precisely
+        assert_eq!(1, text_results.len());
+        let text_result = text_results.get(0).unwrap();
+        let expected_position = 0;
+        let expected_value = Value::new(&String::from(text), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
+
+        assert_eq!(0, integer_results.len());
+
+        assert_eq!(1, float_results.len());
+        let float_result = float_results.get(0).unwrap();
+        let expected_position = 0;
+        let expected_value = Value::new(&String::from(text), &Class::Float).unwrap();
+        assert_eq!(expected_position, float_result.0);
+        assert_eq!(expected_value, float_result.1);
     }
 
     #[test]
     fn to_text() {
         // setup
+        let text0 = "integer";
+        let text2 = "8";
+        let text4 = "and";
+        let text6 = "float";
+        let text8 = "5.5";
         let tokens = vec![
             Token {
                 position: 0,
                 separator: false,
-                text: String::from("integer"),
+                text: String::from(text0),
             },
             Token {
                 position: 1,
@@ -208,7 +252,7 @@ mod tests {
             Token {
                 position: 2,
                 separator: false,
-                text: String::from("8"),
+                text: String::from(text2),
             },
             Token {
                 position: 3,
@@ -218,7 +262,7 @@ mod tests {
             Token {
                 position: 4,
                 separator: false,
-                text: String::from("and"),
+                text: String::from(text4),
             },
             Token {
                 position: 5,
@@ -228,7 +272,7 @@ mod tests {
             Token {
                 position: 6,
                 separator: false,
-                text: String::from("float"),
+                text: String::from(text6),
             },
             Token {
                 position: 7,
@@ -238,20 +282,51 @@ mod tests {
             Token {
                 position: 8,
                 separator: false,
-                text: String::from("5.5"),
+                text: String::from(text8),
             },
         ];
 
         // exercise
-        let text_result = Value::from(&tokens, &Class::Text);
+        let text_results = Value::from(&tokens, &Class::Text);
 
         // verify
-        assert_eq!(5, text_result.len()); // TODO match precisely
+        assert_eq!(5, text_results.len());
+
+        let text_result = text_results.get(0).unwrap();
+        let expected_position = 0;
+        let expected_value = Value::new(&String::from(text0), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
+
+        let text_result = text_results.get(1).unwrap();
+        let expected_position = 2;
+        let expected_value = Value::new(&String::from(text2), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
+
+        let text_result = text_results.get(2).unwrap();
+        let expected_position = 4;
+        let expected_value = Value::new(&String::from(text4), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
+
+        let text_result = text_results.get(3).unwrap();
+        let expected_position = 6;
+        let expected_value = Value::new(&String::from(text6), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
+
+        let text_result = text_results.get(4).unwrap();
+        let expected_position = 8;
+        let expected_value = Value::new(&String::from(text8), &Class::Text).unwrap();
+        assert_eq!(expected_position, text_result.0);
+        assert_eq!(expected_value, text_result.1);
     }
 
     #[test]
     fn to_integer() {
         // setup
+        let text2 = "8";
         let tokens = vec![
             Token {
                 position: 0,
@@ -266,7 +341,7 @@ mod tests {
             Token {
                 position: 2,
                 separator: false,
-                text: String::from("8"),
+                text: String::from(text2),
             },
             Token {
                 position: 3,
@@ -301,15 +376,23 @@ mod tests {
         ];
 
         // exercise
-        let integer_result = Value::from(&tokens, &Class::Integer);
+        let integer_results = Value::from(&tokens, &Class::Integer);
 
         // verify
-        assert_eq!(1, integer_result.len()); // TODO match precisely
+        assert_eq!(1, integer_results.len());
+
+        let integer_result = integer_results.get(0).unwrap();
+        let expected_position = 2;
+        let expected_value = Value::new(&String::from(text2), &Class::Integer).unwrap();
+        assert_eq!(expected_position, integer_result.0);
+        assert_eq!(expected_value, integer_result.1);
     }
 
     #[test]
     fn to_float() {
         // setup
+        let text2 = "8";
+        let text8 = "5.5";
         let tokens = vec![
             Token {
                 position: 0,
@@ -324,7 +407,7 @@ mod tests {
             Token {
                 position: 2,
                 separator: false,
-                text: String::from("8"),
+                text: String::from(text2),
             },
             Token {
                 position: 3,
@@ -354,14 +437,26 @@ mod tests {
             Token {
                 position: 8,
                 separator: false,
-                text: String::from("5.5"),
+                text: String::from(text8),
             },
         ];
 
         // exercise
-        let float_result = Value::from(&tokens, &Class::Float);
+        let float_results = Value::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(2, float_result.len()); // TODO match precisely
+        assert_eq!(2, float_results.len());
+
+        let float_result = float_results.get(0).unwrap();
+        let expected_position = 2;
+        let expected_value = Value::new(&String::from(text2), &Class::Float).unwrap();
+        assert_eq!(expected_position, float_result.0);
+        assert_eq!(expected_value, float_result.1);
+
+        let float_result = float_results.get(1).unwrap();
+        let expected_position = 8;
+        let expected_value = Value::new(&String::from(text8), &Class::Float).unwrap();
+        assert_eq!(expected_position, float_result.0);
+        assert_eq!(expected_value, float_result.1);
     }
 }
