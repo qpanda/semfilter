7184b46390a10fa606c5f786de99ecda35840691
qpanda <7557807+qpanda@users.noreply.github.com>
2021-11-06 18:03:58 +0800

    feat: Introduced concept of Term

--- src/parser.rs
+++ src/parser.rs
@@ -16,6 +16,12 @@ pub enum Value {
     Text(String),
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Term {
+    position: Position,
+    value: Value,
+}
+
 impl Value {
     pub fn new(text: &String, class: &Class) -> Result<Self, Box<dyn Error>> {
         match class {
@@ -30,13 +36,18 @@ impl Value {
             Class::Text => Ok(Value::Text(String::from(text))),
         }
     }
+}
 
-    pub fn from(tokens: &Vec<Token>, class: &Class) -> Vec<(Position, Value)> {
+impl Term {
+    pub fn from(tokens: &Vec<Token>, class: &Class) -> Vec<Term> {
         let mut result = Vec::new();
         for token in tokens {
             if !token.separator {
                 if let Ok(value) = Value::new(&token.text, class) {
-                    result.push((token.position, value));
+                    result.push(Term {
+                        position: token.position,
+                        value: value,
+                    });
                 }
             }
         }
@@ -45,7 +56,7 @@ impl Value {
 }
 
 #[cfg(test)]
-mod tests {
+mod value_tests {
     use super::*;
 
     #[test]
@@ -106,6 +117,11 @@ mod tests {
         assert_eq!(Value::Text(integer_text), ok_2.unwrap());
         assert_eq!(Value::Text(text), ok_3.unwrap());
     }
+}
+
+#[cfg(test)]
+mod term_tests {
+    use super::*;
 
     #[test]
     fn from_separator() {
@@ -118,121 +134,141 @@ mod tests {
         let tokens = vec![separator_token];
 
         // exercise
-        let text_result = Value::from(&tokens, &Class::Text);
-        let integer_result = Value::from(&tokens, &Class::Integer);
-        let float_result = Value::from(&tokens, &Class::Float);
+        let text_terms = Term::from(&tokens, &Class::Text);
+        let integer_terms = Term::from(&tokens, &Class::Integer);
+        let float_terms = Term::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(0, text_result.len());
-        assert_eq!(0, integer_result.len());
-        assert_eq!(0, float_result.len());
+        assert_eq!(0, text_terms.len());
+        assert_eq!(0, integer_terms.len());
+        assert_eq!(0, float_terms.len());
     }
 
     #[test]
     fn from_text() {
         // setup
         let text = "text";
+        let position = 0;
         let text_token = Token {
-            position: 0,
+            position: position,
             separator: false,
             text: String::from(text),
         };
         let tokens = vec![text_token];
 
         // exercise
-        let text_results = Value::from(&tokens, &Class::Text);
-        let integer_results = Value::from(&tokens, &Class::Integer);
-        let float_results = Value::from(&tokens, &Class::Float);
+        let text_terms = Term::from(&tokens, &Class::Text);
+        let integer_terms = Term::from(&tokens, &Class::Integer);
+        let float_terms = Term::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(1, text_results.len());
-        let text_result = text_results.get(0).unwrap();
-        let expected_position = 0;
-        let expected_value = Value::new(&String::from(text), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
+        assert_eq!(1, text_terms.len());
+        assert_eq!(
+            &Term {
+                position: position,
+                value: Value::new(&String::from(text), &Class::Text).unwrap(),
+            },
+            text_terms.get(0).unwrap()
+        );
 
-        assert_eq!(0, integer_results.len());
+        assert_eq!(0, integer_terms.len());
 
-        assert_eq!(0, float_results.len());
+        assert_eq!(0, float_terms.len());
     }
 
     #[test]
     fn from_integer() {
         // setup
         let text = "8";
+        let position = 0;
         let integer_token = Token {
-            position: 0,
+            position: position,
             separator: false,
             text: String::from(text),
         };
         let tokens = vec![integer_token];
 
         // exercise
-        let text_results = Value::from(&tokens, &Class::Text);
-        let integer_results = Value::from(&tokens, &Class::Integer);
-        let float_results = Value::from(&tokens, &Class::Float);
+        let text_terms = Term::from(&tokens, &Class::Text);
+        let integer_terms = Term::from(&tokens, &Class::Integer);
+        let float_terms = Term::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(1, text_results.len());
-        let text_result = text_results.get(0).unwrap();
-        let expected_position = 0;
-        let expected_value = Value::new(&String::from(text), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
+        assert_eq!(1, text_terms.len());
+        assert_eq!(
+            &Term {
+                position: position,
+                value: Value::new(&String::from(text), &Class::Text).unwrap(),
+            },
+            text_terms.get(0).unwrap()
+        );
 
-        assert_eq!(1, integer_results.len());
-        let integer_result = integer_results.get(0).unwrap();
-        let expected_position = 0;
-        let expected_value = Value::new(&String::from(text), &Class::Integer).unwrap();
-        assert_eq!(expected_position, integer_result.0);
-        assert_eq!(expected_value, integer_result.1);
+        assert_eq!(1, integer_terms.len());
+        assert_eq!(
+            &Term {
+                position: position,
+                value: Value::new(&String::from(text), &Class::Integer).unwrap(),
+            },
+            integer_terms.get(0).unwrap()
+        );
 
-        assert_eq!(1, float_results.len());
-        let float_result = float_results.get(0).unwrap();
-        let expected_position = 0;
-        let expected_value = Value::new(&String::from(text), &Class::Float).unwrap();
-        assert_eq!(expected_position, float_result.0);
-        assert_eq!(expected_value, float_result.1);
+        assert_eq!(1, float_terms.len());
+        assert_eq!(
+            &Term {
+                position: position,
+                value: Value::new(&String::from(text), &Class::Float).unwrap(),
+            },
+            float_terms.get(0).unwrap()
+        );
     }
 
     #[test]
     fn from_float() {
         // setup
         let text = "5.5";
+        let position = 0;
         let float_token = Token {
-            position: 0,
+            position: position,
             separator: false,
             text: String::from(text),
         };
         let tokens = vec![float_token];
 
         // exercise
-        let text_results = Value::from(&tokens, &Class::Text);
-        let integer_results = Value::from(&tokens, &Class::Integer);
-        let float_results = Value::from(&tokens, &Class::Float);
+        let text_terms = Term::from(&tokens, &Class::Text);
+        let integer_terms = Term::from(&tokens, &Class::Integer);
+        let float_terms = Term::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(1, text_results.len());
-        let text_result = text_results.get(0).unwrap();
-        let expected_position = 0;
-        let expected_value = Value::new(&String::from(text), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
+        assert_eq!(1, text_terms.len());
+        assert_eq!(
+            &Term {
+                position: position,
+                value: Value::new(&String::from(text), &Class::Text).unwrap(),
+            },
+            text_terms.get(0).unwrap()
+        );
 
-        assert_eq!(0, integer_results.len());
+        assert_eq!(0, integer_terms.len());
 
-        assert_eq!(1, float_results.len());
-        let float_result = float_results.get(0).unwrap();
-        let expected_position = 0;
-        let expected_value = Value::new(&String::from(text), &Class::Float).unwrap();
-        assert_eq!(expected_position, float_result.0);
-        assert_eq!(expected_value, float_result.1);
+        assert_eq!(1, float_terms.len());
+        assert_eq!(
+            &Term {
+                position: position,
+                value: Value::new(&String::from(text), &Class::Float).unwrap(),
+            },
+            float_terms.get(0).unwrap()
+        );
     }
 
     #[test]
     fn to_text() {
         // setup
+        let position0 = 0;
+        let position2 = 2;
+        let position4 = 4;
+        let position6 = 6;
+        let position8 = 8;
         let text0 = "integer";
         let text2 = "8";
         let text4 = "and";
@@ -240,7 +276,7 @@ mod tests {
         let text8 = "5.5";
         let tokens = vec![
             Token {
-                position: 0,
+                position: position0,
                 separator: false,
                 text: String::from(text0),
             },
@@ -250,7 +286,7 @@ mod tests {
                 text: String::from(" "),
             },
             Token {
-                position: 2,
+                position: position2,
                 separator: false,
                 text: String::from(text2),
             },
@@ -260,7 +296,7 @@ mod tests {
                 text: String::from(" "),
             },
             Token {
-                position: 4,
+                position: position4,
                 separator: false,
                 text: String::from(text4),
             },
@@ -270,7 +306,7 @@ mod tests {
                 text: String::from(" "),
             },
             Token {
-                position: 6,
+                position: position6,
                 separator: false,
                 text: String::from(text6),
             },
@@ -280,52 +316,58 @@ mod tests {
                 text: String::from(" "),
             },
             Token {
-                position: 8,
+                position: position8,
                 separator: false,
                 text: String::from(text8),
             },
         ];
 
         // exercise
-        let text_results = Value::from(&tokens, &Class::Text);
+        let text_terms = Term::from(&tokens, &Class::Text);
 
         // verify
-        assert_eq!(5, text_results.len());
-
-        let text_result = text_results.get(0).unwrap();
-        let expected_position = 0;
-        let expected_value = Value::new(&String::from(text0), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
-
-        let text_result = text_results.get(1).unwrap();
-        let expected_position = 2;
-        let expected_value = Value::new(&String::from(text2), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
-
-        let text_result = text_results.get(2).unwrap();
-        let expected_position = 4;
-        let expected_value = Value::new(&String::from(text4), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
-
-        let text_result = text_results.get(3).unwrap();
-        let expected_position = 6;
-        let expected_value = Value::new(&String::from(text6), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
-
-        let text_result = text_results.get(4).unwrap();
-        let expected_position = 8;
-        let expected_value = Value::new(&String::from(text8), &Class::Text).unwrap();
-        assert_eq!(expected_position, text_result.0);
-        assert_eq!(expected_value, text_result.1);
+        assert_eq!(5, text_terms.len());
+        assert_eq!(
+            &Term {
+                position: position0,
+                value: Value::new(&String::from(text0), &Class::Text).unwrap(),
+            },
+            text_terms.get(0).unwrap()
+        );
+        assert_eq!(
+            &Term {
+                position: position2,
+                value: Value::new(&String::from(text2), &Class::Text).unwrap(),
+            },
+            text_terms.get(1).unwrap()
+        );
+        assert_eq!(
+            &Term {
+                position: position4,
+                value: Value::new(&String::from(text4), &Class::Text).unwrap(),
+            },
+            text_terms.get(2).unwrap()
+        );
+        assert_eq!(
+            &Term {
+                position: position6,
+                value: Value::new(&String::from(text6), &Class::Text).unwrap(),
+            },
+            text_terms.get(3).unwrap()
+        );
+        assert_eq!(
+            &Term {
+                position: position8,
+                value: Value::new(&String::from(text8), &Class::Text).unwrap(),
+            },
+            text_terms.get(4).unwrap()
+        );
     }
 
     #[test]
     fn to_integer() {
         // setup
+        let position2 = 2;
         let text2 = "8";
         let tokens = vec![
             Token {
@@ -339,7 +381,7 @@ mod tests {
                 text: String::from(" "),
             },
             Token {
-                position: 2,
+                position: position2,
                 separator: false,
                 text: String::from(text2),
             },
@@ -376,21 +418,24 @@ mod tests {
         ];
 
         // exercise
-        let integer_results = Value::from(&tokens, &Class::Integer);
+        let integer_terms = Term::from(&tokens, &Class::Integer);
 
         // verify
-        assert_eq!(1, integer_results.len());
-
-        let integer_result = integer_results.get(0).unwrap();
-        let expected_position = 2;
-        let expected_value = Value::new(&String::from(text2), &Class::Integer).unwrap();
-        assert_eq!(expected_position, integer_result.0);
-        assert_eq!(expected_value, integer_result.1);
+        assert_eq!(1, integer_terms.len());
+        assert_eq!(
+            &Term {
+                position: position2,
+                value: Value::new(&String::from(text2), &Class::Integer).unwrap(),
+            },
+            integer_terms.get(0).unwrap()
+        );
     }
 
     #[test]
     fn to_float() {
         // setup
+        let position2 = 2;
+        let position8 = 8;
         let text2 = "8";
         let text8 = "5.5";
         let tokens = vec![
@@ -405,7 +450,7 @@ mod tests {
                 text: String::from(" "),
             },
             Token {
-                position: 2,
+                position: position2,
                 separator: false,
                 text: String::from(text2),
             },
@@ -435,28 +480,30 @@ mod tests {
                 text: String::from(" "),
             },
             Token {
-                position: 8,
+                position: position8,
                 separator: false,
                 text: String::from(text8),
             },
         ];
 
         // exercise
-        let float_results = Value::from(&tokens, &Class::Float);
+        let float_terms = Term::from(&tokens, &Class::Float);
 
         // verify
-        assert_eq!(2, float_results.len());
-
-        let float_result = float_results.get(0).unwrap();
-        let expected_position = 2;
-        let expected_value = Value::new(&String::from(text2), &Class::Float).unwrap();
-        assert_eq!(expected_position, float_result.0);
-        assert_eq!(expected_value, float_result.1);
-
-        let float_result = float_results.get(1).unwrap();
-        let expected_position = 8;
-        let expected_value = Value::new(&String::from(text8), &Class::Float).unwrap();
-        assert_eq!(expected_position, float_result.0);
-        assert_eq!(expected_value, float_result.1);
+        assert_eq!(2, float_terms.len());
+        assert_eq!(
+            &Term {
+                position: position2,
+                value: Value::new(&String::from(text2), &Class::Float).unwrap(),
+            },
+            float_terms.get(0).unwrap()
+        );
+        assert_eq!(
+            &Term {
+                position: position8,
+                value: Value::new(&String::from(text8), &Class::Float).unwrap(),
+            },
+            float_terms.get(1).unwrap()
+        );
     }
 }
