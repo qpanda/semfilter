1749cc9a7142500eae69fbe26daf2dabdf712f47
qpanda <7557807+qpanda@users.noreply.github.com>
2021-11-09 21:02:57 +0800

    feat: Taking different approach to define expression grammar

--- src/expression.rs
+++ src/expression.rs
@@ -9,108 +9,95 @@ use crate::tokenizer::Token;
 peg::parser!(grammar expression() for str {
     // TODO return matching positions as set
     // TODO probably don't need test, can make evaluate pub
-    pub rule test(tokens: &Vec<Token>) -> Vec<Position>
-        = evaluate(tokens)
+    pub rule evaluate(tokens: &Vec<Token>) -> Vec<Position>
+        = condition(tokens)
 
     // TODO return matching positions as set
-    rule evaluate(tokens: &Vec<Token>) -> Vec<Position>
-        = class:class() " == " text:text() {?
-            // TODO find way to return more detailed error message
-            let value = Value::new(&text, &class).map_err(|error| "failed to convert")?;
-            let terms = Term::from(tokens, &class);
-            let positions = Evaluator::positions(&terms, |term| term.value == value);
-            return Ok(positions);
-        }
-        / class:class() " != " text:text() {?
-            // TODO find way to return more detailed error message
-            let value = Value::new(&text, &class).map_err(|error| "failed to convert")?;
-            let terms = Term::from(tokens, &class);
-            let positions = Evaluator::positions(&terms, |term| term.value != value);
-            return Ok(positions);
-        }
-        / class:class() " > " text:text() {?
-            // TODO find way to return more detailed error message
-            let value = Value::new(&text, &class).map_err(|error| "failed to convert")?;
-            let terms = Term::from(tokens, &class);
-            let positions = Evaluator::positions(&terms, |term| term.value > value);
-            return Ok(positions);
-        }
-        / class:class() " >= " text:text() {?
-            // TODO find way to return more detailed error message
-            let value = Value::new(&text, &class).map_err(|error| "failed to convert")?;
-            let terms = Term::from(tokens, &class);
-            let positions = Evaluator::positions(&terms, |term| term.value >= value);
-            return Ok(positions);
-        }
-        / class:class() " < " text:text() {?
-            // TODO find way to return more detailed error message
-            let value = Value::new(&text, &class).map_err(|error| "failed to convert")?;
-            let terms = Term::from(tokens, &class);
-            let positions = Evaluator::positions(&terms, |term| term.value < value);
-            return Ok(positions);
-        }
-        / class:class() " <= " text:text() {?
-            // TODO find way to return more detailed error message
-            let value = Value::new(&text, &class).map_err(|error| "failed to convert")?;
-            let terms = Term::from(tokens, &class);
-            let positions = Evaluator::positions(&terms, |term| term.value <= value);
-            return Ok(positions);
+    rule condition(tokens: &Vec<Token>) -> Vec<Position>
+        = integer_condition(tokens)
+        / float_condition(tokens)
+        / text_condition(tokens)
+
+    //
+    // conditions
+    //
+    rule integer_condition(tokens: &Vec<Token>) -> Vec<Position>
+    = integers:integers(tokens) " == " integer:integer() { matching_positions(&integers, |term| term.value == integer) }
+    / integers:integers(tokens) " != " integer:integer() { matching_positions(&integers, |term| term.value != integer) }
+    / integers:integers(tokens) " > " integer:integer() { matching_positions(&integers, |term| term.value > integer) }
+    / integers:integers(tokens) " >= " integer:integer() { matching_positions(&integers, |term| term.value >= integer) }
+    / integers:integers(tokens) " < " integer:integer() { matching_positions(&integers, |term| term.value < integer) }
+    / integers:integers(tokens) " <= " integer:integer() { matching_positions(&integers, |term| term.value <= integer) }
+
+    rule float_condition(tokens: &Vec<Token>) -> Vec<Position>
+    = floats:floats(tokens) " == " float:float() { matching_positions(&floats, |term| term.value == float) }
+    / floats:floats(tokens) " != " float:float() { matching_positions(&floats, |term| term.value != float) }
+    / floats:floats(tokens) " > " float:float() { matching_positions(&floats, |term| term.value > float) }
+    / floats:floats(tokens) " >= " float:float() { matching_positions(&floats, |term| term.value >= float) }
+    / floats:floats(tokens) " < " float:float() { matching_positions(&floats, |term| term.value < float) }
+    / floats:floats(tokens) " <= " float:float() { matching_positions(&floats, |term| term.value <= float) }
+
+    rule text_condition(tokens: &Vec<Token>) -> Vec<Position>
+    = texts:texts(tokens) " == " text:text() { matching_positions(&texts, |term| term.value == text) }
+    / texts:texts(tokens) " != " text:text() { matching_positions(&texts, |term| term.value != text) }
+    / texts:texts(tokens) " > " text:text() { matching_positions(&texts, |term| term.value > text) }
+    / texts:texts(tokens) " >= " text:text() { matching_positions(&texts, |term| term.value >= text) }
+    / texts:texts(tokens) " < " text:text() { matching_positions(&texts, |term| term.value < text) }
+    / texts:texts(tokens) " <= " text:text() { matching_positions(&texts, |term| term.value <= text) }
+
+    //
+    // terms
+    //
+    rule integers(tokens: &Vec<Token>) -> Vec<Term>
+        = "integer" { Term::from(tokens, &Class::Integer) }
+
+    rule floats(tokens: &Vec<Token>) -> Vec<Term>
+        = "float" { Term::from(tokens, &Class::Float) }
+
+    rule texts(tokens: &Vec<Token>) -> Vec<Term>
+        = "text" { Term::from(tokens, &Class::Text) }
+
+    //
+    // values
+    //
+    // TODO check internet for correct pattern for integers
+    rule integer() -> Value
+        = n:$(['0'..='9']+) {?
+            Value::new(&String::from(n), &Class::Integer).map_err(|error| "failed to parse integer")
         }
 
-    // TODO break this down into one rule per class so that we can use individual rules in evaluate
-    rule class() -> Class
-        = "integer" { return Class::Integer }
-        / "float" { return Class::Float }
-        / "text" { return Class::Text }
-        / expected!("class")
+    // TODO check internet for correct pattern for floats
+    rule float() -> Value
+        = n:$(['0'..='9']+"."['0'..='9']+) {?
+            Value::new(&String::from(n), &Class::Float).map_err(|error| "failed to parse float")
+        }
 
-    rule text() -> String
-        // TODO generalize expression (everything between separators as per Tokenizer)
-        = (n:$(['a'..='z' | '0'..='9']+) {
-            return String::from(n);
-        })
-        / expected!("text")
+    // TODO is identifier, do we want general text?
+    rule text() -> Value
+        = n:$(['a'..='z'|'A'..='Z']['a'..='z'|'A'..='Z'|'0'..='9']+) {?
+            Value::new(&String::from(n), &Class::Text).map_err(|error| "failed to parse text")
+        }
 });
 
-struct Evaluator {}
-
-impl Evaluator {
-    fn positions<P>(terms: &Vec<Term>, predicate: P) -> Vec<Position>
-    where
-        P: FnMut(&&Term) -> bool,
-    {
-        return terms
-            .into_iter()
-            .filter(predicate)
-            .map(|term| term.position)
-            .collect::<Vec<Position>>();
-    }
+fn matching_positions<P>(terms: &Vec<Term>, predicate: P) -> Vec<Position>
+where
+    P: FnMut(&&Term) -> bool,
+{
+    terms
+        .into_iter()
+        .filter(predicate)
+        .map(|term| term.position)
+        .collect::<Vec<Position>>()
 }
 
 #[cfg(test)]
-mod tests_expression {
+mod tests {
     use super::*;
 
     #[test]
-    fn ttt() {
-        let tokens = vec![Token {
-            position: 0,
-            separator: false,
-            text: String::from("9"),
-        }];
-        assert_eq!(expression::test("integer == 9", &tokens), Ok(vec![0]));
-        assert_eq!(expression::test("integer != 9", &tokens), Ok(vec![]));
-    }
-}
-
-#[cfg(test)]
-mod tests_evaluator {
-    use super::*;
-
-    #[test]
-    fn positions() {
+    fn matching_integer_positions() {
         // setup
-        let integer_terms = vec![
+        let integers = vec![
             Term {
                 position: 2,
                 value: Value::Integer(1),
@@ -126,23 +113,81 @@ mod tests_evaluator {
         ];
 
         // exercise
-        let positions_integer_eq_0 = Evaluator::positions(&integer_terms, |term| term.value == Value::Integer(0));
-        let positions_integer_eq_2 = Evaluator::positions(&integer_terms, |term| term.value == Value::Integer(2));
-        let positions_integer_ne_0 = Evaluator::positions(&integer_terms, |term| term.value != Value::Integer(0));
-        let positions_ne_integer_2 = Evaluator::positions(&integer_terms, |term| term.value != Value::Integer(2));
-        let positions_integer_gt_0 = Evaluator::positions(&integer_terms, |term| term.value > Value::Integer(0));
-        let positions_integer_st_0 = Evaluator::positions(&integer_terms, |term| term.value < Value::Integer(0));
-        let positions_float_eq_2 = Evaluator::positions(&integer_terms, |term| term.value == Value::Float(2.0));
-        let positions_float_gt_0 = Evaluator::positions(&integer_terms, |term| term.value > Value::Float(0.0));
+        let integers_eq_integer_0 = matching_positions(&integers, |term| term.value == Value::Integer(0));
+        let integers_eq_integer_2 = matching_positions(&integers, |term| term.value == Value::Integer(2));
+        let integeres_ne_integer_0 = matching_positions(&integers, |term| term.value != Value::Integer(0));
+        let integers_ne_integer_2 = matching_positions(&integers, |term| term.value != Value::Integer(2));
+        let integers_gt_integer_0 = matching_positions(&integers, |term| term.value > Value::Integer(0));
+        let integers_lt_integer_0 = matching_positions(&integers, |term| term.value < Value::Integer(0));
+        let integers_eq_float_2 = matching_positions(&integers, |term| term.value == Value::Float(2.0));
+        let integers_gt_float_0 = matching_positions(&integers, |term| term.value > Value::Float(0.0));
+        let integers_eq_text_2 = matching_positions(&integers, |term| term.value > Value::Text(String::from("2")));
 
         // verify
-        assert_eq!(0, positions_integer_eq_0.len());
-        assert_eq!(vec![4], positions_integer_eq_2);
-        assert_eq!(vec![2, 4, 6], positions_integer_ne_0);
-        assert_eq!(vec![2, 6], positions_ne_integer_2);
-        assert_eq!(vec![2, 4, 6], positions_integer_gt_0);
-        assert_eq!(0, positions_integer_st_0.len());
-        assert_eq!(0, positions_float_eq_2.len());
-        assert_eq!(0, positions_float_gt_0.len());
+        assert_eq!(0, integers_eq_integer_0.len());
+        assert_eq!(vec![4], integers_eq_integer_2);
+        assert_eq!(vec![2, 4, 6], integeres_ne_integer_0);
+        assert_eq!(vec![2, 6], integers_ne_integer_2);
+        assert_eq!(vec![2, 4, 6], integers_gt_integer_0);
+        assert_eq!(0, integers_lt_integer_0.len());
+        assert_eq!(0, integers_eq_float_2.len());
+        assert_eq!(0, integers_gt_float_0.len());
+        assert_eq!(0, integers_eq_text_2.len());
+    }
+
+    #[test]
+    fn invalid_integer_conditions() {
+        assert!(expression::evaluate("integer + 9", &vec![]).is_err());
+        assert!(expression::evaluate("wrong == 9", &vec![]).is_err());
+        assert!(expression::evaluate("integer == a", &vec![]).is_err());
+    }
+
+    #[test]
+    fn valid_integer_conditions() {
+        assert!(expression::evaluate("integer == 9", &vec![]).is_ok());
+        assert!(expression::evaluate("integer != 9", &vec![]).is_ok());
+        assert!(expression::evaluate("integer > 9", &vec![]).is_ok());
+        assert!(expression::evaluate("integer >= 9", &vec![]).is_ok());
+        assert!(expression::evaluate("integer < 9", &vec![]).is_ok());
+        assert!(expression::evaluate("integer <= 9", &vec![]).is_ok());
+    }
+
+    #[test]
+    fn evaluate_expression_no_tokens() {
+        assert_eq!(expression::evaluate("integer == 9", &vec![]), Ok(vec![]));
+        assert_eq!(expression::evaluate("integer != 9", &vec![]), Ok(vec![]));
+        assert_eq!(expression::evaluate("float > 1.0", &vec![]), Ok(vec![]));
+    }
+
+    #[test]
+    fn evaluate_expression_with_tokens() {
+        // setup
+        let tokens = vec![
+            Token {
+                position: 0,
+                separator: false,
+                text: String::from("a1"),
+            },
+            Token {
+                position: 1,
+                separator: false,
+                text: String::from("9"),
+            },
+            Token {
+                position: 2,
+                separator: false,
+                text: String::from("5.5"),
+            },
+        ];
+
+        assert_eq!(expression::evaluate("integer == 9", &tokens), Ok(vec![1]));
+        assert_eq!(expression::evaluate("integer != 9", &tokens), Ok(vec![]));
+        assert_eq!(expression::evaluate("float == 5.5", &tokens), Ok(vec![2]));
+        assert_eq!(expression::evaluate("float != 5.5", &tokens), Ok(vec![1]));
+        assert_eq!(expression::evaluate("float > 0.0", &tokens), Ok(vec![1, 2]));
+        assert_eq!(expression::evaluate("float < 0.0", &tokens), Ok(vec![]));
+        assert_eq!(expression::evaluate("text == a1", &tokens), Ok(vec![0]));
+        assert_eq!(expression::evaluate("text != a1", &tokens), Ok(vec![1, 2]));
+        assert_eq!(expression::evaluate("text == b1", &tokens), Ok(vec![]));
     }
 }
