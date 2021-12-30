adac43c10ec36a1846e7c32ba80f85d4b606b373
qpanda <7557807+qpanda@users.noreply.github.com>
2021-12-30 17:13:02 +0800

    feat: Handling platform specific code correctly so that it builds on both Windows and macOS

---
+++ src/ansi.rs
@@ -0,0 +1,39 @@
+use anyhow::Error;
+
+use crate::filter::Settings;
+
+//
+// Windows specific code
+//
+
+#[cfg(target_family = "windows")]
+use anyhow::anyhow;
+
+#[cfg(target_family = "windows")]
+pub fn enable_ansi_support(settings: &Settings) -> Result<(), Error> {
+    if cfg!(windows) {
+        if settings.mode.is_highlight() {
+            match ansi_term::enable_ansi_support() {
+                Ok(()) => return Ok(()),
+                Err(error) => return Err(anyhow!("enabling failed with error code {}", error)),
+            }
+        }
+    }
+
+    Ok(())
+}
+
+//
+// Unix specific code
+//
+
+#[cfg(target_family = "unix")]
+pub fn enable_ansi_support(settings: &Settings) -> Result<(), Error> {
+    if cfg!(windows) {
+        if settings.mode.is_highlight() {
+            // ANSI support needs to be enabled only on Windows
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+mod ansi;
 mod arguments;
 mod expression;
 mod filter;
 mod parser;
 mod tokenizer;
 
-use anyhow::{anyhow, Context, Error};
+use anyhow::{Context, Error};
 
 use crate::arguments::Arguments;
 use crate::filter::Filter;
-use crate::filter::Settings;
 use crate::tokenizer::Tokenizer;
 
 fn main() -> Result<(), Error> {
@@ -16,24 +16,11 @@ fn main() -> Result<(), Error> {
     let tokenizer = Tokenizer::new(args.separators).context("Initializing tokenizer failed")?;
     let filter = Filter::new(&tokenizer, &args.expression, &args.settings).context("Initializing filter failed")?;
 
-    enable_ansi_support(&args.settings).context("Enabling ANSI support failed")?;
+    ansi::enable_ansi_support(&args.settings).context("Enabling ANSI support failed")?;
 
     filter
         .filter(&mut args.input, &mut args.output)
         .context("Filtering failed")?;
-
-    Ok(())
-}
-
-fn enable_ansi_support(settings: &Settings) -> Result<(), Error> {
-    if cfg!(windows) {
-        if settings.mode.is_highlight() {
-            match ansi_term::enable_ansi_support() {
-                Ok(()) => return Ok(()),
-                Err(error) => return Err(anyhow!("enabling failed with error code {}", error)),
-            }
-        }
-    }
 
     Ok(())
 }
